1bc0fa2ddb0c81c9bad666865d34b813d9a1ab92
andresa321 <andresaofc14@gmail.com>
2025-04-29 16:28:45 -0300

    o commit anterior comentado

--- exercicio_05.py
+++ exercicio_05.py
@@ -2,12 +2,13 @@
 notas=[""]*5
 soma=0
 contador=0
+#codigo for para calcular notas
 for x in range(len(notas)):
     notas[x]= float(input("digite a nota: "))
     soma = soma + notas[x]
-
+#variavel para media
 media = soma/len(notas)
-
+#for para saber se notas é maior que media
 for x in range(len(notas)):
     if notas[x] > media:
         contador += 1
